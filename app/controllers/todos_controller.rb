e4429153e2ae87b428aaf7fe2febbf2020af8b2a
GuipOliveira <guipeoliveira@gmail.com>
2018-05-07 16:57:17 -0300

    Make app production test

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,55 @@
+class TodosController < ApplicationController
+
+before_action :set_todo, only: [:edit, :update, :show, :destroy]
+
+    def index
+        @todos = Todo.all
+    end
+
+    def new
+        @todo = Todo.new
+    end
+   
+    def create 
+        @todo = Todo.new(todo_params)
+        if @todo.save  
+            flash[:notice] = "Todo was created sucessfully!!"
+            redirect_to todo_path(@todo)          
+        else
+            render "new"
+        end
+    end
+
+    def show
+    end
+
+    def edit
+    end
+    
+    def update 
+        if @todo.update(todo_params)
+            flash[:notice] = "Todo was sucessfully updated"
+            redirect_to todo_path(@todo)
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy 
+        @todo.destroy
+        flash[:notice] = "Todo sucessfully deleted"
+        redirect_to todos_path
+    end
+
+
+    private 
+    
+    def todo_params
+        params.require(:todo).permit(:name, :description)
+    end
+
+    def set_todo
+        @todo = Todo.find(params[:id])
+    end
+
+end
